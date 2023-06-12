6c2b747544efa364059fa25d7472f9696593dc62
Luka Najjar <46554723+fly04@users.noreply.github.com>
2023-06-12 12:06:17 +0200

    Implement first gameplay loop

--- Psychopomp/Assets/Scripts/TargetController.cs
+++ Psychopomp/Assets/Scripts/TargetController.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class TargetController : MonoBehaviour
 {
 
-    [SerializeField] bool isGap = false;
+    enum TargetType
+    {
+        Gap,
+        Start,
+        End
+    }
+
+    [SerializeField] TargetType targetType;
     [SerializeField] GameObject UnitPrefab;
     [SerializeField] float zOffset = 30.5f;
     [SerializeField] Transform spawnPosition;
@@ -19,36 +26,66 @@ public class TargetController : MonoBehaviour
         {
             Destroy(gameObject);
 
-            if (isGap)
+            switch (targetType)
             {
-                // Get parent component that have the tag Unit
-                GameObject Unit = transform.parent.gameObject.transform.parent.gameObject;
-
-                // Instantiate new unit with an offset relative to the unit parent
-                GameObject newUnit = Instantiate(UnitPrefab, Unit.transform.position + new Vector3(0, 0, zOffset), Unit.transform.rotation);
-
-                // Add the new unit to the UnitController
-                UnitController.Instance.addUnit(newUnit);
-
-                // Get all newUnit children that have the tag target
-                findNextTargets(newUnit.transform);
-
-                // Order the targets by their id
-                nextTargets.Sort((x, y) => x.GetComponent<TargetController>().id.CompareTo(y.GetComponent<TargetController>().id));
-
-                // Add each target to the boat target list
-                foreach (GameObject target in nextTargets)
-                {
-                    BoatController.Instance.addTarget(target);
-                }
-            }
-            else
-            {
-                // Set boat to is waiting
-                // BoatController.Instance.setIsWaiting(true);
-                Instantiate(soulPrefab, spawnPosition.position, Quaternion.identity);
+                case TargetType.Gap:
+                    handleGapEnter();
+                    break;
+                case TargetType.Start:
+                    handleStartEnter();
+                    break;
+                case TargetType.End:
+                    handleEndEnter();
+                    break;
             }
         }
+    }
+
+    void handleGapEnter()
+    {
+        // Get parent component that have the tag Unit
+        GameObject Unit = transform.parent.gameObject.transform.parent.gameObject;
+
+        // Instantiate new unit with an offset relative to the unit parent
+        GameObject newUnit = Instantiate(UnitPrefab, Unit.transform.position + new Vector3(0, 0, zOffset), Unit.transform.rotation);
+
+        // Add the new unit to the UnitController
+        UnitController.Instance.addUnit(newUnit);
+
+        // Get all newUnit children that have the tag target
+        findNextTargets(newUnit.transform);
+
+        // Order the targets by their id
+        nextTargets.Sort((x, y) => x.GetComponent<TargetController>().id.CompareTo(y.GetComponent<TargetController>().id));
+
+        // Add each target to the boat target list
+        foreach (GameObject target in nextTargets)
+        {
+            BoatController.Instance.addTarget(target);
+        }
+    }
+
+    void handleStartEnter()
+    {
+        BoatController.Instance.setIsWaiting(true);
+        Instantiate(soulPrefab, spawnPosition.position, Quaternion.identity);
+    }
+
+    void handleEndEnter()
+    {
+        // Make the Boat wait
+        BoatController.Instance.setIsWaiting(true);
+
+        // Find the active Soul
+        SoulController soul = GameObject.FindGameObjectWithTag("Soul").GetComponent<SoulController>();
+
+        // Find EndPosition sibling
+        Transform endPosition = transform.parent.Find("EndPosition");
+        soul.setEndPosition(endPosition);
+
+
+        // Make the soul in its leaveBoat state
+        soul.prepareToLeave();
     }
 
     void findNextTargets(Transform parent)
