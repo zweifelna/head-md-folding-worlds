1fa979e13eae51175101cbab99843b24b2060fc8
Luka Najjar <46554723+fly04@users.noreply.github.com>
2023-06-14 15:08:30 +0200

    Add wind

---
+++ Psychopomp/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        play("Wind");
+    }
+
+    public void play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    //to use sound
+    //FindObjectOfType<AudioManager>().Play("sound name");
+}
